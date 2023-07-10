e3a60cb43461fa0057473cda4531e81c2165be18
Aryuemaan Kumar Chowdhury <91824434+aryuemaan@users.noreply.github.com>
2023-07-10 14:45:02 +0530

    add this file to your repo

---
+++ architecture.bash
@@ -0,0 +1,21 @@
+├── data
+│   ├── train
+│   │   ├── happy
+│   │   ├── sad
+│   │   ├── angry
+│   │   └── ...
+│   └── test
+│       ├── happy
+│       ├── sad
+│       ├── angry
+│       └── ...
+├── models
+│   ├── emotion_detection_model.py
+│   └── ...
+├── utils
+│   ├── data_preprocessing.py
+│   └── ...
+├── train.py
+├── test.py
+├── README.md
+└── requirements.txt
